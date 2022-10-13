1f12cebd692796570a33e02c7b2f0fd256b9afdb
ryumhan <ryum7han@gmail.com>
2022-10-13 22:38:29 +0900

    [practice] Modules and Dependency Injection
    - using module method, nest app can seperate each module

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/Movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -42,7 +43,7 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  path(@Param('id') movieid: number, @Body() updateData) {
+  path(@Param('id') movieid: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieid, updateData);
   }
 }
